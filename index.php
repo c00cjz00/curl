cb4f0518bd200134c6e373d224c933a273b9f8bb
c00cjz00 <summerhill001@gmail.com>
2018-10-16 15:56:40 +0800

    init

--- index.php
+++ index.php
@@ -4,24 +4,21 @@ $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 
 
+
 // Check if file already exists
-if ((substr($target_file,-10,10)=="qstatQuery") || (substr($target_file,-9,9)=="qstatList")){
+if ((substr($target_file,-6,6)==".Query") || (substr($target_file,-5,5)==".List")){
  $uploadOk = 1;
 }else{
  echo "Sorry, file format is not allowed.";    
  $uploadOk = 0;
 }
 
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-        
+
 // Check if file already exists
-if (file_exists($target_file)) {
+/*if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
-}
+}*/
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
@@ -34,6 +31,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
+    unlink($target_file); sleep(1);
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
--- qstat.php
+++ qstat.php
@@ -1,18 +1,18 @@
 <?php
-date_default_timezone_set("Asia/Taipei");
-$today = date("YmdHi");
-$qstatQueryFile="/tmp/".$today.".qstatQuery";
-$qstatListFile="/tmp/".$today.".qstatList";
-$cmd="qstat -Q > ".$qstatQueryFile;
-passthru($cmd);
-$cmd="qstat > ".$qstatListFile;
-passthru($cmd);
-sleep(1);
-$cmd="curl -F 'fileToUpload=@".$qstatQueryFile."' http://140.110.17.247/github/curl/index.php";
-passthru($cmd);
-$cmd="curl -F 'fileToUpload=@".$qstatListFile."' http://140.110.17.247/github/curl/index.php";
-passthru($cmd);
-sleep(1);
-unlink($qstatQueryFile);
-unlink($qstatListFile);
+# date_default_timezone_set("Asia/Taipei");
+# $today = date("YmdHi");
+# $today  = mktime(date("H"), date("i"), 0, date("m")  , date("d"), date("Y"));
+
+$qstatQueryFile="/home/c00cjz00/tmp/qstat.Query";
+$qstatListFile="/home/c00cjz00/tmp/qstat.List";
+unlink($qstatQueryFile); sleep(1);
+$cmd="/opt/pbs/bin/qstat -Q > ".$qstatQueryFile; exec($cmd); sleep(1);
+unlink($qstatListFile); sleep(1);
+$cmd="/opt/pbs/bin/qstat > ".$qstatListFile; exec($cmd); sleep(1);
+$cmd="/usr/bin/curl -F 'fileToUpload=@".$qstatQueryFile."' http://140.110.17.247/github/curl/index.php";
+echo $cmd."\n"; exec($cmd); sleep(1);
+$cmd="/usr/bin/curl -F 'fileToUpload=@".$qstatListFile."' http://140.110.17.247/github/curl/index.php";
+echo $cmd."\n"; exec($cmd); sleep(1);
+
+
 ?>
